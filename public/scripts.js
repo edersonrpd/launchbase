9cdeb1f6078e64357cf776e3224224171fa939a4
Ederson <edersonrp@gmail.com>
2020-06-15 20:18:11 -0300

    commit

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,8 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 
 // função para pegar  o id do video e inserir no modal
+/* Modal Desativado
+
 for (let card of cards) {
   card.addEventListener("click", () => {
     const videoID = card.getAttribute("id");
@@ -9,6 +11,13 @@ for (let card of cards) {
     modalOverlay.querySelector(
       "iframe"
     ).src = `https://www.youtube.com/embed/${videoID}`;
+  });
+}*/
+
+for (let card of cards) {
+  card.addEventListener("click", () => {
+    const videoID = card.getAttribute("id");
+    window.location.href = `/video?id=${videoID}`
   });
 }
 
--- server.js
+++ server.js
@@ -7,8 +7,11 @@ const videos = require("./data");  //data.js onde esta o array dos videos e ID d
 server.use(express.static("public")); //pasta dos arquivos estaticos
 server.set("view engine", "njk");
 
-nunjucks.configure("views", { express: server,
-   autoescape:false})  //Fazer o nunjuck interpretar html 
+nunjucks.configure("views", { 
+   express: server,   //setando o express como template engine
+   autoescape:false,  //Fazer o nunjuck interpretar html 
+   noCache: true //
+  })  
 
 server.get("/", (req, res) => {
   const about = {
@@ -30,6 +33,20 @@ server.get("/", (req, res) => {
 server.get("/portfolio", (req, res) => {
   return res.render("portfolio", {items: videos}); //chamando a pagina de portofolio e o array da data.js
 })
+
+//Criando a pagina de videos
+server.get("/video", (req,res) => {
+  const id = req.query.id
+  const video = videos.find((video) =>{
+    return video.id == id
+   
+  })
+  if (!video) {
+    return res.send("Video not found")
+  }
+  return res.render("video", {item: video})
+})
+
 //configurando porta de escuta 3000
 server.listen(3000, () => {
   console.log("Servidor Iniciado");
